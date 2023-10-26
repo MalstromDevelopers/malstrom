d09991518ee4966206c6a2c7b575344993a5c040
Damion Werner <damion.werner@gmail.com>
2023-10-26 19:49:27 +0200

    maybe working union

--- src/union.rs
+++ src/union.rs
@@ -1,5 +1,5 @@
 use timely::Data;
-use timely::dataflow::operators
+use timely::dataflow::operators::Operator;
 use crate::{JetStream, DataUnion};
 
 pub trait UnionStream<'scp, D1: Data, D2: Data> {
@@ -15,6 +15,13 @@ use timely::dataflow::channels::pact::Pipeline;
 use timely::dataflow::{Stream, Scope};
 
 /// Merge the contents of two streams.
+/// 
+/// Both streams must return Option<D> to allow the
+/// other stream to progress if there is no data on one
+/// of them. I.e. if a stream has no data at the moment, but
+/// might have more data later, it shall yield None.
+/// 
+/// If both streams have data, union will alternate between them
 trait Union<G: Scope, D1: Data, D2: Data> {
     fn union(&self, left: &Stream<G, D1>, right: &Stream<G, D2>) -> Stream<G, DataUnion<D1, D2>>;
 }
@@ -23,34 +30,44 @@ trait Union<G: Scope, D1: Data, D2: Data> {
 impl<G: Scope, D1: Data, D2: Data> Union<G, D1, D2> for G {
     fn union(&self, left: &Stream<G, D1>, right: &Stream<G, D2>) -> Stream<G, DataUnion<D1, D2>>
     {
+        left.binary(right, Pipeline, Pipeline, "union", |default_cap, _info| {
+                 let mut cap = Some(default_cap);
+                 let mut vec1: Vec<D1> = Vec::new();
+                 let mut vec2: Vec<D2> = Vec::new();
 
-        // create an operator builder.
-        use timely::dataflow::operators::generic::builder_rc::OperatorBuilder;
-        let mut builder = OperatorBuilder::new("Union".to_string(), self.clone());
 
-        // create new input handles for each input stream.
-        let mut handle_a = builder.new_input(&left, Pipeline);
-        let mut handle_b = builder.new_input(&right, Pipeline);
+                 // flip flop to alternate between inputs
+                 let mut call_1 = true;
 
-        // create one output handle for the concatenated results.
-        let (mut output, result) = builder.new_output();
-        handle_a.
-        // build an operator that plays out all input data.
-        builder.build(move |_capability| {
+                 move |input1, input2, output| {
+                    
+                    if call_1 {
+                        if let Some((time, data)) = input1.next(){
+                            data.swap(&mut vec1);
+                            let mut as_output: Vec<DataUnion<D1, D2>> = vec1.into_iter().map(
+                                |x| DataUnion::Left(x)
+                            ).collect();
 
-            let mut vector = Vec::new();
-            move |_frontier| {
-                let mut output = output.activate();
-                if handle_a.next()
-                for handle in handles.iter_mut() {
-                    handle.for_each(|time, data| {
-                        data.swap(&mut vector);
-                        output.session(&time).give_vec(&mut vector);
-                    })
-                }
-            }
-        });
+                            if as_output.len() != 0 {
+                                output.session(&time).give_vec(&mut as_output);
+                                call_1 = !call_1;
+                                return;
+                            }
+                        }
+                    } 
+                    if let Some((time, data)) = input2.next(){
+                        data.swap(&mut vec2);
+                        let mut as_output: Vec<DataUnion<D1, D2>> = vec1.into_iter().map(
+                            |x| DataUnion::Left(x)
+                        ).collect();
 
-        result
+                        if as_output.len() != 0 {
+                            output.session(&time).give_vec(&mut as_output);
+                            call_1 = !call_1;
+                            return;
+                        }
+                    }
+                 }
+             })
     }
 }
