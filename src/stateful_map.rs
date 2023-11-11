83d875256ca3f3af852d7b5437559de357bf3d81
Nico Rumsch <nico@notvads.ovh>
2023-11-11 21:49:07 +0100

    update stateful map

--- src/stateful_map.rs
+++ src/stateful_map.rs
@@ -1,14 +1,17 @@
-use crate::stream::{dist_rand, Data, JetStream, StandardOperator};
+use crate::stream::{
+    jetstream::{Data, JetStreamBuilder},
+    operator::StandardOperator,
+};
 
 pub trait StatefulMap<I> {
     fn stateful_map<O: Data, S: 'static>(
         self,
         mapper: impl FnMut(I, &mut S) -> O + 'static,
         state_backend: S,
-    ) -> JetStream<O>;
+    ) -> JetStreamBuilder<O>;
 }
 
-impl<O> StatefulMap<O> for JetStream<O>
+impl<O> StatefulMap<O> for JetStreamBuilder<O>
 where
     O: Data,
 {
@@ -16,20 +19,16 @@ where
         self,
         mut mapper: impl FnMut(O, &mut S) -> T + 'static,
         mut state_backend: S,
-    ) -> JetStream<T>
-    {
-        let operator = StandardOperator::new(
-            move |inputs: &Vec<crossbeam::channel::Receiver<O>>, outputs, _| {
-                let inp = inputs.iter().filter_map(|x| x.try_recv().ok());
-                let mut data = Vec::new();
-                for x in inp {
-                    // this is super weird, but I could not get the code to borrow
-                    // check otherwise
-                    data.push(mapper(x, &mut state_backend))
-                }
-                dist_rand(data.into_iter(), outputs)
-            },
-        );
+    ) -> JetStreamBuilder<T> {
+        let operator = StandardOperator::new(move |input, output, frontier| {
+            // since this operator does not participate in progress tracking
+            // it must set u64::MAX to not block others from advancing
+            // TODO: Should the stateful map participate?
+            let _ = frontier.advance_to(u64::MAX);
+            if let Some(msg) = input.recv() {
+                output.send(mapper(msg, &mut state_backend));
+            }
+        });
         self.then(operator)
     }
 }
@@ -38,7 +37,7 @@ where
 mod tests {
     use std::collections::HashMap;
 
-    use crate::{frontier::FrontierHandle, worker::Worker};
+    use crate::{frontier::FrontierHandle, worker::Worker, stream::jetstream::JetStreamEmpty, map::Map};
 
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
@@ -47,58 +46,28 @@ mod tests {
     #[test]
     fn test_stateful_map() {
         let mut worker = Worker::new();
-        let state: HashMap<u8, f64> = HashMap::new();
+        let state: HashMap<u8, i32> = HashMap::new();
 
         let source = |frontier_handle: &mut FrontierHandle| {
             frontier_handle.advance_by(1).unwrap();
-            Some(rand::random::<f64>())
+            Some(1)
         };
-        let stream = JetStream::new()
+        let stream = JetStreamEmpty::new()
             .source(source)
             .stateful_map(
                 |x, state| {
-                    let new_val = state.get(&0).unwrap_or(&0.0)+ x;
+                    let new_val = state.get(&0).unwrap_or(&0) + x;
                     state.insert(0, new_val);
-                    x
+                    new_val
                 },
                 state,
             )
-            .finalize();
-        let probe = stream.get_probe();
+            .map(|o| {
+                assert_eq!(o, 1);})
+            .build();
         worker.add_stream(stream);
-        assert_eq!(probe.read(), 0);
-        worker.step();
-        assert_eq!(probe.read(), 1);
-
-        assert_eq!(worker.get_frontier().unwrap(), 1);
-    }
-
-    #[test]
-    fn test_persistent_map() {
-        let mut worker = Worker::new();
-        let state: HashMap<u8, f64> = HashMap::new();
-
-        let source = |frontier_handle: &mut FrontierHandle| {
-            frontier_handle.advance_by(1).unwrap();
-            Some(rand::random::<f64>())
-        };
-        let stream = JetStream::new()
-            .source(source)
-            .stateful_map(
-                |x, state| {
-                    let new_val = state.get(&0).unwrap_or(&0.0)+ x;
-                    state.insert(0, new_val);
-                    x
-                },
-                state,
-            )
-            .finalize();
-        let probe = stream.get_probe();
-        worker.add_stream(stream);
-        assert_eq!(probe.read(), 0);
-        worker.step();
-        assert_eq!(probe.read(), 1);
-
-        assert_eq!(worker.get_frontier().unwrap(), 1);
+        worker.step(); // source 
+        worker.step(); // stateful map
+        worker.step(); // map
     }
 }
