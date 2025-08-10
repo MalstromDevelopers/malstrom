3acaec5ff7ffab92a9844eb7182c0772d0f69425
Nico Rumsch <nico@acupof.xyz>
2025-08-10 16:18:54 +0200

    implement sliding window

--- malstrom-windows/src/lib.rs
+++ malstrom-windows/src/lib.rs
@@ -1,2 +1,2 @@
 pub mod tumbling;
-// pub mod sliding;
+pub mod sliding;
---
+++ malstrom-windows/src/sliding.rs
@@ -0,0 +1,220 @@
+use std::{
+    hash::Hash,
+    ops::{Add, Div, Rem, Sub},
+};
+
+use malstrom::{
+    channels::operator_io::Output,
+    operators::{StatefulLogic, StatefulOp, expiremap::ExpireMap, indexmap::IndexMap},
+    runtime::communication::Distributable,
+    stream::StreamBuilder,
+    types::{DataMessage, Key, MaybeData, Message, Timestamp},
+};
+use num_traits::CheckedSub;
+use serde::{Serialize, de::DeserializeOwned};
+
+struct SlidingWindowOp<F, S> {
+    aggregator: F,
+    size: S,
+}
+impl<F, S> SlidingWindowOp<F, S> {
+    fn new(function: F, size: S) -> Self {
+        Self {
+            aggregator: function,
+            size,
+        }
+    }
+}
+
+impl<F, K, VI, T, VO> StatefulLogic<K, VI, T, VO, ExpireMap<T, VI, T>> for SlidingWindowOp<F, T>
+where
+    K: Key,
+    VO: MaybeData,
+    VI: Serialize + DeserializeOwned,
+    T: Timestamp
+        + Add<Output = T>
+        + Sub<Output = T>
+        + CheckedSub<Output = T>
+        + Div<Output = T>
+        + Eq
+        + Hash
+        + Clone
+        + Serialize
+        + DeserializeOwned
+        + 'static,
+    F: FnMut(&[&VI]) -> VO + 'static,
+{
+    fn on_data(
+        &mut self,
+        msg: DataMessage<K, VI, T>,
+        mut key_state: ExpireMap<T, VI, T>,
+        output: &mut Output<K, VO, T>,
+    ) -> Option<ExpireMap<T, VI, T>> {
+        key_state.insert(
+            msg.timestamp.clone(),
+            msg.value,
+            // Keep for double the size/duration to ensure its available on epoch
+            msg.timestamp.clone() + self.size.clone() + self.size.clone(),
+        );
+
+        // Need to prevent a negative wrap therefore do a checked sub
+        let lower = msg
+            .timestamp
+            .clone()
+            // subtract size - 1
+            .checked_sub(&(self.size.clone() - (self.size.clone() / self.size.clone())))
+            .unwrap_or(T::MIN);
+        let window = key_state.get_range(|k, _| *k < lower, |k, _| *k <= msg.timestamp.clone());
+
+        if let Some(window) = window {
+            let values = window.values().map(|o| &o.value).collect::<Vec<_>>();
+
+            output.send(Message::Data(DataMessage::new(
+                msg.key.clone(),
+                (self.aggregator)(&values),
+                msg.timestamp.clone(),
+            )));
+        }
+
+        Some(key_state)
+    }
+
+    fn on_epoch(
+        &mut self,
+        epoch: &T,
+        state: &mut IndexMap<K, ExpireMap<T, VI, T>>,
+        _output: &mut Output<K, VO, T>,
+    ) {
+        state.retain(|_, v| {
+            v.expire(epoch);
+            !v.is_empty()
+        });
+    }
+}
+
+pub trait SlidingWindow<K, V, T, VO> {
+    /// Sliding event time window which passes a reference of all values part of the window
+    /// on epoch update to the aggregator function and emit a record.
+    ///
+    /// Note: THe step size is determined by the frequency of emitted epochs.
+    ///
+    /// # Example
+    /// ```rust
+    /// use malstrom::operators::*;
+    /// use malstrom::runtime::SingleThreadRuntime;
+    /// use malstrom::snapshot::NoPersistence;
+    /// use malstrom::sources::{SingleIteratorSource, StatelessSource};
+    /// use malstrom::worker::StreamProvider;
+    /// use malstrom::sinks::{VecSink, StatelessSink};
+    /// use malstrom_windows::sliding::SlidingWindow;
+    ///
+    /// let sink = VecSink::new();
+    /// let sink_clone = sink.clone();
+    ///
+    /// SingleThreadRuntime::builder()
+    ///     .persistence(NoPersistence)
+    ///     .build(move |provider: &mut dyn StreamProvider| {
+    ///         let (on_time, _late) = provider
+    ///             .new_stream()
+    ///             .source("source", StatelessSource::new(SingleIteratorSource::new(0..3)))
+    ///             .assign_timestamps("assigner", |msg| msg.timestamp)
+    ///             .generate_epochs("generate", |_, t| t.to_owned());
+    ///
+    ///         on_time
+    ///             .key_local("key", |_| false)
+    ///             .sliding_window("count", 2, |x| x.iter().copied().sum())
+    ///             .sink("sink", StatelessSink::new(sink_clone));
+    ///      })
+    ///      .execute()
+    ///      .unwrap();
+    /// let expected: Vec<i32> = vec![(0..1).sum(), (0..2).sum(), (1..3).sum()];
+    /// let out: Vec<i32> = sink.into_iter().map(|x| x.value).collect();
+    /// assert_eq!(out, expected);
+    /// ```
+    fn sliding_window(
+        self,
+        name: &str,
+        size: T,
+        aggregator: impl (FnMut(&[&V]) -> VO) + 'static,
+    ) -> StreamBuilder<K, VO, T>;
+}
+
+impl<K, V, T, VO> SlidingWindow<K, V, T, VO> for StreamBuilder<K, V, T>
+where
+    K: Key + Distributable,
+    V: MaybeData + Distributable,
+    VO: MaybeData + Distributable,
+    T: Timestamp
+        + Distributable
+        + Add<Output = T>
+        + Sub<Output = T>
+        + CheckedSub<Output = T>
+        + Div<Output = T>
+        + Rem<Output = T>
+        + Hash
+        + Eq
+        + Copy,
+{
+    fn sliding_window(
+        self,
+        name: &str,
+        size: T,
+        aggregator: impl (FnMut(&[&V]) -> VO) + 'static,
+    ) -> StreamBuilder<K, VO, T> {
+        self.stateful_op(name, SlidingWindowOp::new(aggregator, size))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use malstrom::{
+        keyed::KeyLocal,
+        operators::{AssignTimestamps, GenerateEpochs, Sink, Source},
+        runtime::SingleThreadRuntime,
+        sinks::{StatelessSink, VecSink},
+        snapshot::NoPersistence,
+        sources::{SingleIteratorSource, StatelessSource},
+        worker::StreamProvider,
+    };
+
+    use crate::sliding::SlidingWindow;
+
+    #[test]
+    fn test_sliding_window() {
+        let collector = VecSink::new();
+
+        let rt = SingleThreadRuntime::builder()
+            .persistence(NoPersistence)
+            .build(|provider: &mut dyn StreamProvider| {
+                let (on_time, _late) = provider
+                    .new_stream()
+                    .source(
+                        "source",
+                        StatelessSource::new(SingleIteratorSource::new([
+                            "one".to_owned(),
+                            "two".to_owned(),
+                            "three".to_owned(),
+                            "four".to_owned(),
+                        ])),
+                    )
+                    .assign_timestamps("assigner", |msg| msg.timestamp)
+                    .generate_epochs("generate", |_, t| t.to_owned());
+
+                on_time
+                    .key_local("key", |_| false)
+                    .sliding_window("join-two", 2, |x| {
+                        x.iter()
+                            .cloned()
+                            .cloned()
+                            .collect::<Vec<String>>()
+                            .join("|")
+                    })
+                    .sink("sink", StatelessSink::new(collector.clone()));
+            });
+        rt.execute().unwrap();
+        assert_eq!(
+            collector.into_iter().map(|x| x.value).collect::<Vec<_>>(),
+            vec!["one", "one|two", "two|three", "three|four"]
+        );
+    }
+}
